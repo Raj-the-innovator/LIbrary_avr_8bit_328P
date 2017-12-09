794a846426c50810d810e8d9b0e8c8a5b9b6d54d
Rajesh Jat <30384033+Raj-the-innovator@users.noreply.github.com>
2017-12-09 14:21:08 +0530

    pwm 1 added

---
+++ pwm1_16bit.h
@@ -0,0 +1,14 @@
+/*
+ * pwm_8bit.h
+ *
+ * Created: 12/6/2017 2:59:44 PM
+ *  Author: Administrator
+ */ 
+
+
+#ifndef PWM_8BIT_H_
+#define PWM_8BIT_H_
+
+void pwm_int();
+
+#endif /* PWM_8BIT_H_ */
